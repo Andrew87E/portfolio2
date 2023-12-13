ee48b89176c32e645ca287870176f680083c5489
Andrew Edwards <aedwards@tifco.com>
2023-12-13 13:55:05 -0600

    added footer component

---
+++ components/helpers/hover.tsx
@@ -0,0 +1,24 @@
+import { Tooltip as ReactTooltip } from "react-tooltip";
+import React from "react";
+
+export const Hover = (props: {
+  backgroundColor: string;
+  textColor: string;
+  id: string;
+  name:
+    | string
+    | number
+    | boolean
+    | React.ReactElement<any, string | React.JSXElementConstructor<any>>
+    | React.ReactFragment
+    | React.ReactPortal;
+}) => {
+  return (
+    <ReactTooltip
+      id={props.id}
+      className="text-lg transition-transform duration-1000"
+    >
+      <span>{props.name}</span>
+    </ReactTooltip>
+  );
+};
