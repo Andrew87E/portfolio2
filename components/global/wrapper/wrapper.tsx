143a7948aa6c6de68fd668f77c8df4ffa72e6975
Andrew Edwards <aedwards@tifco.com>
2023-12-19 22:18:48 -0600

    animate nav

--- components/global/wrapper/wrapper.tsx
+++ components/global/wrapper/wrapper.tsx
@@ -4,6 +4,7 @@ import { Footer } from "../footer/footer";
 import { Github } from "./githubButton";
 import { MobileMenu } from "../navbar/mobileMenu";
 import { Navbar } from "../navbar/navbar";
+import { motion, useAnimation } from "framer-motion";
 
 type PageProps = {
   currentPage: string;
@@ -17,12 +18,36 @@ type PageProps = {
 export const Page = ({ currentPage, meta: { desc }, children }: PageProps) => {
   const [darkModeActual, setDarkMode] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const controls = useAnimation();
 
   const pageTitle = `${
     currentPage === "Home"
       ? "Andrew Edwards - Web Developer"
       : `${currentPage} - Edwards.codes`
   }`;
+
+  const handleScroll = () => {
+    const currentScrollY = window.scrollY;
+
+    if (currentScrollY > lastScrollY) {
+      // Scrolling down
+      controls.start({ top: "-60px" }); // Adjust '-60px' to match the height of your navbar
+    } else {
+      // Scrolling up
+      controls.start({ top: "0px" });
+    }
+
+    setLastScrollY(currentScrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [lastScrollY, controls]);
 
   const handleDarkChange = () => {
     localStorage.setItem("color-theme", darkModeActual ? "light" : "dark");
@@ -40,24 +65,26 @@ export const Page = ({ currentPage, meta: { desc }, children }: PageProps) => {
   };
 
   useEffect(() => {
-    // check if user has set a preference for dark mode
-    const localPref = localStorage.getItem("color-theme");
-    const userPrefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
-
-    if (userPrefersDark || localPref === "dark") {
-      setDarkMode(true);
-      localStorage.setItem("color-theme", "dark");
-      document.body.classList.remove("bg-slate-200");
-      document.body.classList.add("dark");
-      document.body.classList.add("bg-gray-800");
-    } else {
-      setDarkMode(false);
-      localStorage.setItem("color-theme", "light");
-      document.body.classList.add("bg-slate-200");
-      document.body.classList.remove("dark");
-      document.body.classList.remove("bg-gray-800");
+    if (typeof window !== "undefined") {
+      // we only want to run this on initial page load
+      // check if user has set a preference for dark mode
+      const localPref = localStorage.getItem("color-theme");
+      const userPrefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      if (userPrefersDark || localPref === "dark") {
+        setDarkMode(true);
+        localStorage.setItem("color-theme", "dark");
+        document.body.classList.remove("bg-slate-200");
+        document.body.classList.add("dark");
+        document.body.classList.add("bg-gray-800");
+      } else {
+        setDarkMode(false);
+        localStorage.setItem("color-theme", "light");
+        document.body.classList.add("bg-slate-200");
+        document.body.classList.remove("dark");
+        document.body.classList.remove("bg-gray-800");
+      }
     }
   }, []);
 
@@ -73,7 +100,19 @@ export const Page = ({ currentPage, meta: { desc }, children }: PageProps) => {
       </Head>
 
       <main>
-        <nav className="fixed top-0 right-0 left-0 h-16 inline-flex  items-center justify-between z-30 bg-lightbg dark:bg-darkbg">
+        <motion.nav
+          animate={controls}
+          initial={{ top: "0px" }}
+          transition={{
+            type: "spring",
+            stiffness: 100,
+            damping: 20,
+            delay: 0,
+            duration: 0.5,
+          }}
+          className={`fixed top-0 right-0 left-0 h-16 inline-flex  items-center justify-between z-30 bg-lightbg dark:bg-darkbg `}
+          id="navBar"
+        >
           <div className="hidden md:inline-flex w-full items-center justify-between pb-2">
             <Navbar
               currentPage={currentPage}
@@ -90,7 +129,7 @@ export const Page = ({ currentPage, meta: { desc }, children }: PageProps) => {
               setShowMenu={setShowMenu}
             />
           </div>
-        </nav>
+        </motion.nav>
 
         {children}
       </main>
